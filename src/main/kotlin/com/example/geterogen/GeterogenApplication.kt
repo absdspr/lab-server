c408fba344570a43b47f0e90935cb28b1f8594ce
lighrik <kiriys36@gmail.com>
2021-02-12 10:23:29 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/geterogen/GeterogenApplication.kt
@@ -0,0 +1,11 @@
+package com.example.geterogen
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeterogenApplication
+
+fun main(args: Array<String>) {
+    runApplication<GeterogenApplication>(*args)
+}
